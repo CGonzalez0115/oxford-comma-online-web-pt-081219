63249cf2637617f2dc2e0a5c515ad10ad432df68
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 08:38:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,15 @@
 def oxford_comma(array)
   ["kiwi"]
   => "kiwi"
-
 end
+  "kiwi", "durian".split("and")
+    => "kiwi and durian"
+end
+  "kiwi", "durian", "starfruit".split(",","and")
+  => "kiwi, durian, and starfruit"
+end
+  "kiwi", "durian", "starfruit", "mangos", "dragon fruits".split(",", "and")
+  => "kiwi, durian, starfruit, mangos, and dragon fruits"
+
+  "kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos".split(",", "and")
+  => "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
